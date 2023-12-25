18bac341709796e88e2a048cb46567cbe80665fe
Danh Vo Hong Phuc <20520275@gm.uit.edu.vn>
2023-12-25 15:16:32 +0700

    new2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("feature");
+        System.out.println("feature1");
         System.out.println("Hello world!");
         System.out.println("Main commit 1");
         System.out.println("Main commit 2");
