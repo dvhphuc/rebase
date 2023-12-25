8ece7d118288a39740ffd74d878bedaf8b4ceff7
Danh Vo Hong Phuc <20520275@gm.uit.edu.vn>
2023-12-25 14:58:04 +0700

    cm2-main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Main commit 1");
+        System.out.println("Main commit 2");
     }
 }
